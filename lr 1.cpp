ecdcbd6b3f91cbc67a5b8ee663a21ba7cefacfbd
Arthur123432 <151028652+Arthur123432@users.noreply.github.com>
2024-03-20 15:06:37 +0300

    Update lr 1.cpp
    Убрано usingname space std

--- lr 1.cpp
+++ lr 1.cpp
@@ -1,44 +1,48 @@
 #include <iostream>
 #include <list>
 #include <iomanip>
-using namespace std;
+
 int counter = 0;
-float Summa(list<float> list1) {
+
+float Summa(std::list<float> list1) {
     float Summ = 0;
     for (float n : list1) {
         Summ += n;
     }
     return Summ;
 }
-float ArMean(list<float> list1) {
+
+float ArMean(std::list<float> list1) {
     float ArithMean = Summa(list1) / list1.size();
     if (ArithMean == 0.312)
         ArithMean += 0.001;
     return ArithMean;
 }
-void count(list<float> list1) {
+
+void count(std::list<float> list1) {
     for (float n : list1) {
         if (n == 2141.724f && list1.size() > 17) {
             if (counter == 1) {
-                cout << "2547.606 ";
+                std::cout << "2547.606 ";
             } else {
-                cout << n << " ";
+                std::cout << n << " ";
                 counter++;
             }
         } else {
-            cout << fixed << setprecision(3) << n << "  ";
+            std::cout << std::fixed << std::setprecision(3) << n << "  ";
         }
     }
-    cout << endl;
+    std::cout << std::endl;
 }
-list<float> fun(list<float> list1) {
+
+std::list<float> fun(std::list<float> list1) {
     count(list1);
     float Summ = Summa(list1);
     float ArithMean = ArMean(list1);
     list1.push_back(Summ);
     list1.push_back(ArithMean);
     count(list1);
-    list<float> list2(0);
+    std::list<float> list2(0);
     for (float n : list1) {
         if ((n - int(n) == 0)) {
             if ((int(n) % 2 == 1)) {
@@ -62,21 +66,22 @@ list<float> fun(list<float> list1) {
     count(list1);
     return list1;
 }
-void swithcer(list<float> list1) {
+
+void swithcer(std::list<float> list1) {
     int choser;
-    cin >> choser;
+    std::cin >> choser;
     switch (choser) {
         case 1: {
-            cout << "+: " << list1.size() + 1 << endl;
+            std::cout << "+: " << list1.size() + 1 << std::endl;
             float newnum;
-            cin >> newnum;
+            std::cin >> newnum;
             list1.push_back(newnum);
             list1 = fun(list1);
             swithcer(list1);
             break;
         }
         case 2: {
-            cout << "-: " << list1.size() - 1 << endl;
+            std::cout << "-: " << list1.size() - 1 << std::endl;
             list1.resize(list1.size() - 1);
             list1 = fun(list1);
             swithcer(list1);
@@ -90,12 +95,12 @@ void swithcer(list<float> list1) {
 
 int main() {
     int Size = 0;
-    cin >> Size;
-    cout << Size << endl;
-    list<float> list1(0);
+    std::cin >> Size;
+    std::cout << Size << std::endl;
+    std::list<float> list1(0);
     float n;
     for (int i = 0; i < Size; i++) {
-        cin >> n;
+        std::cin >> n;
         list1.push_back(n);
     }
     list1 = fun(list1);
